5bd2b0a3193ef5a9607b0868b8428e5729ae7caa
Dustin <dustin.holm@gmx.net>
2023-02-13 22:39:51 +0100

    init

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import copy from "rollup-plugin-copy";
+import css from "rollup-plugin-css-only";
+import clear from "rollup-plugin-delete";
+import livereload from "rollup-plugin-livereload";
+import serve from "rollup-plugin-serve";
+import svelte from "rollup-plugin-svelte";
+import { swc } from "rollup-plugin-swc3";
+
+const isDev = process.env.NODE_ENV === "development";
+const isTauri = process.env.MODE === "tauri";
+
+export default {
+    input: "src/main.js",
+    output: {
+        name: "TicToc",
+        file: "target/tictoc.js",
+        format: "iife",
+    },
+    watch: {
+        include: ["src/**"],
+        clearScreen: false,
+    },
+    plugins: [
+        clear({ targets: "target/*", runOnce: true }),
+        commonjs(),
+        copy({ targets: [{ src: "public/*.html", dest: "target" }] }),
+        css({ output: "style.css" }),
+        nodeResolve({ browser: true }),
+        replace({
+            preventAssignment: true,
+            values: {
+                "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+                "process.env.MODE": JSON.stringify(process.env.MODE),
+            },
+        }),
+        svelte({ emitCss: true, include: "src/**/*.svelte" }),
+        swc({ minify: !isDev }),
+        isDev && serve({ open: !isTauri, contentBase: "target", port: "3000" }),
+        isDev && livereload({ watch: "target", verbose: true }),
+    ],
+};
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import App from "./App.svelte";
+
+const main = new App({ target: document.body });
+
+export default main;
