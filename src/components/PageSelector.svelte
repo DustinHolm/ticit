deee158ab45502c3b01a431261d2d87b21448fa1
Dustin <dustin.holm@gmx.net>
2023-03-12 22:51:12 +0100

    switch between pages + multi-page view

---
+++ src/components/PageSelector.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let onPrevious;
+    export let previousPossible;
+    export let onNext;
+    export let nextPossible;
+</script>
+
+<div>
+    <button on:click={onPrevious} disabled={!previousPossible}>Left</button>
+    <button on:click={onNext} disabled={!nextPossible}>Right</button>
+</div>
--- src/layouts/HorizontalLayout.svelte
+++ src/layouts/HorizontalLayout.svelte
@@ -3,9 +3,27 @@
     import DailyData from "../components/DailyData.svelte";
     import DailyOverviewPage from "../pages/DailyOverviewPage.svelte";
     import EntryPage from "../pages/EntryPage.svelte";
+    import PageSelector from "../components/PageSelector.svelte";
+
+    let currentPage = 0;
+    let width;
+
+    const onNext = () => (currentPage = currentPage + 1);
+    const onPrevious = () => (currentPage = currentPage - 1);
+
+    $: maxPages = Math.floor(width / 800);
+    $: nextPossible = currentPage < 1 && maxPages < 2;
+    $: previousPossible = currentPage > 0 && maxPages < 2;
 </script>
+
+<svelte:window bind:innerWidth={width} />
 
 <DaySelector />
 <DailyData />
-<EntryPage />
-<DailyOverviewPage />
+{#if currentPage === 0 || maxPages > 1}
+    <EntryPage />
+{/if}
+{#if currentPage === 1 || maxPages > 1}
+    <DailyOverviewPage />
+{/if}
+<PageSelector {onNext} {nextPossible} {onPrevious} {previousPossible} />
